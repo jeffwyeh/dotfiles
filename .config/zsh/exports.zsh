9bf8928ee93840a8d6b860b3a4132aa6f3c74cf6
Jeff Yeh <yehyeh@amazon.com>
2021-07-23 12:59:15 -0700

    Add Python3.8 to path and update oh-my-zsh

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,4 +1,4 @@
 # Add toolbox
 # Add git helper scripts
-export PATH=$HOME/.config/git/bin:$HOME/.toolbox/bin:$PATH
+export PATH=$HOME/.config/git/bin:$HOME/.toolbox/bin:$HOME/Library/Python/3.8/bin:$PATH
 
