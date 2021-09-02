fb6264e4d046806d870d684fd72b0db5e028657a
Jeff Yeh <yehyeh@amazon.com>
2021-09-02 15:32:28 +0000

    Add envImprovement to PATH

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -2,3 +2,6 @@
 # Add git helper scripts
 export PATH=$HOME/.config/git/bin:$HOME/.toolbox/bin:$HOME/Library/Python/3.8/bin:$PATH
 
+# Add envImprovement
+export PATH=/apollo/env/envImprovement/bin:$PATH
+
