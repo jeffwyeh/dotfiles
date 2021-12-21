9996fb875f0fac78876645e7518e48dfc96e0464
Jeff Yeh <yehyeh@amazon.com>
2021-12-21 09:15:38 -0700

    Update sketchybar battery colors

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -5,17 +5,26 @@ BATT_INFO=$(system_profiler SPPowerDataType -json)
 CHARGED=$(echo $BATT_INFO | jq '.SPPowerDataType[].sppower_battery_charge_info.sppower_battery_fully_charged // empty')
 CHARGING=$(echo $BATT_INFO | jq '.SPPowerDataType[].sppower_battery_is_charging // empty')
 CONNECTED=$(echo $BATT_INFO | jq '.SPPowerDataType[].sppower_battery_charger_connected // empty')
+PERCENT=$(echo $BATT_INFO | jq '.SPPowerDataType[].sppower_battery_charge_info.sppower_battery_state_of_charge // empty')
 
 if [[ $CONNECTED == "\"TRUE\"" ]]; then
     ICON=""
-else
+elif [[ $PERCENT -ge 90 ]]; then
+    ICON=""
+elif [[ $PERCENT -ge 65 ]]; then
     ICON=""
+elif [[ $PERCENT -ge 35 ]]; then
+    ICON=""
+elif [[ $PERCENT -ge 10 ]]; then
+    ICON=""
+else
+    ICON=""
 fi
 
 if [[ $CHARGING == "\"TRUE\"" ]]; then
-    COLOR=0xff72e0c5
-elif [[ $CONNECTED == "\"TRUE\"" ]]; then
     COLOR=0xffffc663
+elif [[ $CONNECTED == "\"TRUE\"" ]]; then
+    COLOR=0xff72e0c5
 else
     COLOR=0xffff6e70
 fi
