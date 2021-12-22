002882f84de8be76094ed86660e38af8cee36d28
Jeff Yeh <yehyeh@amazon.com>
2021-12-21 17:36:45 -0700

    Add monitoring scripts for sketchybar

---
+++ .config/sketchybar/plugins/disk.sh
@@ -0,0 +1,2 @@
+sketchybar -m --set disk_percent label=$(df -lh | grep /dev/disk1s1 | awk '{ printf("%02.0f%%\n", $5) }')
+
---
+++ .config/sketchybar/plugins/ram.sh
@@ -0,0 +1,2 @@
+sketchybar -m --set ram_percent label=$(memory_pressure | grep "System-wide memory free percentage:" | awk '{ printf("%02.0f%%\n", 100-$5"%") }')
+
