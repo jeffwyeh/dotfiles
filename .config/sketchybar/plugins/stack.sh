955a7d57d8ca49da35024b43af6cad362220461c
Jeff Yeh <yehyeh@amazon.com>
2021-12-28 18:06:10 -0700

    Updates to yabai stack indicator

--- .config/sketchybar/plugins/stack.sh
+++ .config/sketchybar/plugins/stack.sh
@@ -4,26 +4,25 @@
 CURRENT=$(yabai -m query --windows --window | jq '.["stack-index"]')
 LAST=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
 if [[ $LAST -eq 0 ]]; then
-   sketchybar -m --set stack label="" \
-                 --set stack label.font="Hack Nerd Font:Regular:12.0"
+   sketchybar -m --set stack label=""
 else
    # Use Numbers in place of Dots if the Stack is greater than 3
    # Use a larger font for the unicode dots
    if [[ $LAST -gt 3 ]]; then
      sketchybar -m --set stack label=$(printf "[%s/%s]" "$CURRENT" "$LAST") \
-                   --set stack label.font="Hack Nerd Font:Regular:12.0" \
+                   --set stack label.font="Helvetica Neue:Bold:12.0" \
                    --set stack drawing=on
    else
       # Create Stack Indicator
       declare -a dots=()
       for i in $(seq 0 $(expr $LAST - 1))
       do  
-        [[ $( expr $CURRENT - 1) -eq $i ]] && dots+="● " || dots+="○ "
+        [[ $( expr $CURRENT - 1) -eq $i ]] && dots+=" " || dots+=" "
       done
 
       # Display Indicator
       sketchybar -m --set stack label="$(echo $dots)" \
-                    --set stack label.font="Hack Nerd Font:Regular:12.0" \
+                    --set stack label.font="Font Awesome 5 Free:Regular:12.0" \
                     --set stack drawing=on
    fi
 fi
