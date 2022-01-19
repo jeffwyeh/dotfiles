a60cedd16020e5020bc873c0b11efda7e7f0c200
Jeff Yeh <yehyeh@amazon.com>
2022-01-19 08:27:43 -0700

    Move jenv init out of zshrc and into aliases to make it MacOS specific

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -5,6 +5,29 @@ if [ "$system_type" = "Darwin" ]; then
    # Running on mac OS
    alias initall='kinit && mwinit -s --aea'
    alias resetsb='brew services stop spacebar && sleep 2 && brew services start spacebar'
+
+   # jenv setup
+   eval export PATH="/Users/yehyeh/.jenv/shims:${PATH}"
+   export JENV_SHELL=zsh
+   export JENV_LOADED=1
+   unset JAVA_HOME
+   source '/usr/local/Cellar/jenv/0.5.4/libexec/libexec/../completions/jenv.zsh'
+   jenv rehash 2>/dev/null
+   jenv refresh-plugins
+   jenv() {
+     typeset command
+     command="$1"
+     if [ "$#" -gt 0 ]; then
+       shift
+     fi
+
+     case "$command" in
+     enable-plugin|rehash|shell|shell-options)
+       eval `jenv "sh-$command" "$@"`;;
+     *)
+       command jenv "$command" "$@";;
+     esac
+   }
 elif [ "$system_type" = "Linux" ]; then
    # Running on AL2
    alias initall='kinit && mwinit -o -s'
