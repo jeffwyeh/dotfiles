1b649533e129b11a885b3e8e321a13550a62e5cd
Jeff Yeh <yehyeh@amazon.com>
2022-03-02 11:12:36 -0700

    Update sketchybar yabai float indicator due to yabai update

--- .config/sketchybar/plugins/yabai_float.sh
+++ .config/sketchybar/plugins/yabai_float.sh
@@ -1,10 +1,11 @@
 # Show an icon for the float status of the selected window
 
-case "$(yabai -m query --windows --window | jq .floating)" in
-    0)
-    sketchybar -m --set yabai_float icon=""
-    ;;
-    1)
-    sketchybar -m --set yabai_float icon=""
-    ;;
+case "$(yabai -m query --windows --window | jq '."is-floating"')" in
+    false)
+      sketchybar -m --set yabai_float icon=""
+      ;;
+    true)
+      sketchybar -m --set yabai_float icon=""
+      ;;
 esac
+
