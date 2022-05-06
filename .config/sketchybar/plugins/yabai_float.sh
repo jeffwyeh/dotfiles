9c9e28a393b50d4ab119763c587e611c31265dc6
Jeff Yeh <yehyeh@amazon.com>
2022-05-06 09:24:23 -0700

    Update sketchybar to show fullscreen status of window

--- .config/sketchybar/plugins/yabai_float.sh
+++ .config/sketchybar/plugins/yabai_float.sh
@@ -1,10 +1,17 @@
 # Show an icon for the float status of the selected window
 
 case "$(yabai -m query --windows --window | jq '."is-floating"')" in
-    false)
-      sketchybar -m --set yabai_float icon=""
+   false)
+      case "$(yabai -m query --windows --window | jq '."has-fullscreen-zoom"')" in
+         false)
+            sketchybar -m --set yabai_float icon=""
+            ;;
+         true)
+            sketchybar -m --set yabai_float icon=""
+            ;;
+      esac
       ;;
-    true)
+   true)
       sketchybar -m --set yabai_float icon=""
       ;;
 esac
