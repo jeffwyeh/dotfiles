3feb3b3500dd9ecb2fc30d10babbb1423d063280
Jeff Yeh <yehyeh@amazon.com>
2023-05-22 09:22:09 -0700

    Update JSONToString

--- .config/boop/JSONToString.js
+++ .config/boop/JSONToString.js
@@ -10,11 +10,11 @@
 **/
 
 function main(input) {
-   input.text = input.text.replace(/\"/g, '\\"');
    try {
       input.text = JSON.stringify(JSON.parse(input.text));
    } catch (error) {
       input.postError("Invalid JSON");
    }
+   input.text = input.text.replace(/\"/g, '\\"');
 }
 
