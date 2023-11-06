6af432a9911e59ecd75e963869d1612c445e8c48
Jeff Yeh <yehyeh@amazon.com>
2023-11-06 13:06:53 -0700

    Minor fixes for Yabai v6.0.0

--- .config/sketchybar/items/float.sh
+++ .config/sketchybar/items/float.sh
@@ -9,4 +9,4 @@ float=(
 
 sketchybar --add item yabai_float left \
            --set yabai_float "${float[@]}" \
-           --subscribe yabai_float front_app_switched space_change display_change mouse.clicked float_change
+           --subscribe yabai_float front_app_switched space_change display_change float_change window_focus
